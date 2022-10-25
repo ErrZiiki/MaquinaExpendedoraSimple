84fa059edc420ae63423384b65441253c83d0346
Diego de la Fuente <diegodelafuentealonso@gmail.com>
2022-10-25 22:29:32 +0200

    Maquina expendedora simple con precio origen y llegada fijos

--- MaquinaExpendedoraSimple.java
+++ MaquinaExpendedoraSimple.java
@@ -1,34 +1,24 @@
 public class MaquinaExpendedoraSimple {
     
     // El precio del billete
-    private int precioBillete;
+    private int precioBillete = 20;
     // La cantidad de dinero que lleva metida el cliente actual
     private int balanceClienteActual;
     // El total de dinero almacenado en la maquina desde su ultimo vaciado
     private int totalDineroAcumulado;
     // El origen del billete
-    private String estacionOrigen;
+    private String estacionOrigen = "León";
     // El destino del billete
-    private String estacionDestino;
+    private String estacionDestino = "La Bañeza";
 
     /**
      * Crea una maquina expendedora de billetes de tren con el 
      * precio del billete y el origen y destino dados. Se asume que el precio
      * del billete que se recibe es mayor que 0.
      */
-    public MaquinaExpendedoraSimple(int precioDelBillete, String origen, String destino) {
-        precioBillete = precioDelBillete;
+    public MaquinaExpendedoraSimple() {
         balanceClienteActual = 0;
         totalDineroAcumulado = 0;
-        estacionOrigen = origen;
-        estacionDestino = destino;
-    }
-
-    /**
-     * Devuelve el precio del billete
-     */
-    public int getPrecioBillete() {
-        return precioBillete;
     }
 
     /**
